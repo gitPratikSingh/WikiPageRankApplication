49ad6a201708e15df570f889a048027c22e74159
Pratik <pratik.iem01@gmail.com>
2017-09-07 10:07:03 -0400

    Update wikiPageRank.py

--- wikiPageRank.py
+++ wikiPageRank.py
@@ -7,61 +7,58 @@ import itertools
 from operator import add
 from pyspark import SparkContext
 
+# import the parser
+from sql-to-csv-parser.parse_wiki import *
 
-def funcParse(urls):
-	"""Parses a urls pair string into urls pair."""
-	parts = urls.split(': ') 
-	return parts[0], parts[1]
-    
+# import the helper functions  computeContribs and iteratehelper
+from pagerank_helper_functions import *
 
-def funcContri(urls, rank):
-    	"""Calculates URL contributions to the rank of other URLs."""
-    	urls = urls.split(' ')
-    	num_urls = len(urls)
-    	for url in urls:
-        	yield (url, rank / num_urls)
+# parse the page file, and generate a csv file 'output/page.csv'
+parse_file('wikidumps/simplewiki-20170901-page.sql', 'output')
 
-if __name__ == "__main__":
-	if len(sys.argv) != 3:
-		print("Please check the Usage: pagerank <file> <iterations>", file=sys.stderr)
-        exit(-1)
-	
-    # Initializing the spark context.
-    sc = SparkContext(appName="PythonPageRank")
+# parse the page file, and generate a csv file 'output/pagelinks.csv'
+parse_file('wikidumps/simplewiki-20170901-pagelinks.sql', 'output')
 
-    #1 means the number of partitions
-    rddline = sc.textFile(sys.argv[1], 1)
-	
-    # Separate each element in the list into a tuple
-    outlinks = rddline.map(lambda urls: funcParse(urls))
-	
-    # Loads all URLs with other URL(s) link to from input file and initialize ranks of them to one.
-    ranks = outlinks.map(lambda url_neighbors: (url_neighbors[0], 1.0))
-	
-    	#mylist = list(set([ranks]))
-    	#mylist.append
-	#count = int(sys.argv[2])
-	
-    # Calculates and updates URL ranks continuously using PageRank algorithm.
-    for iteration in range(int(sys.argv[2])):
-        contribs = outlinks.join(ranks).flatMap(
-            lambda url_urls_rank: funcContri(url_urls_rank[1][0], url_urls_rank[1][1]))
 
-        #Taxation based calculations
-        #ranks = contribs.reduceByKey(add).mapValues(lambda rank: rank * 0.85 + 0.15)
-        
-        #Idealized based calculations
-        ranks = contribs.reduceByKey(add).mapValues(lambda rank: rank)
-	
-	#Add rankings to Titles.txt
-	
-	titles = sc.textFile('/Lab1/titles-sorted.txt', 1)
-	index = sc.parallelize(range(1, 5716809), 1)
-	#inp = sc.textFile('/Lab1/titles-sorted.txt').zipWithIndex()
-	tmpf = index.zip(titles)
-	
-	filemap = tmpf.map(lambda (index, titles): (str(index), titles))
-	finale = filemap.join(ranks).map(lambda (index, ran): ran).sortBy(lambda a: a[1], False)
-		
-	finale.saveAsTextFile("outputfile")
-	sc.stop()
+# Initializing the spark context.
+sc = SparkContext(appName="PythonPageRank")
+
+#create a pageRdd and filter the pages in namesspace 0
+pageRdd = (sc.textFile('page.csv')
+             .map(lambda line:line.split(','))
+	     .filter(lambda x:x[1]=='0')
+	     .map(lambda x:(x[0],x[2]))
+	     .cache())
+
+#create a linksFrmToRdd and filter the pageslinks in namesspace 0
+linksFrmToRdd=(sc.textFile('pagelinks.csv')
+		 .map(lambda line:line.split(','))
+		 .filter(lambda x:x[1]=='0' and x[3]=='0')
+		 .map(lambda x:(x[0].replace('\"',''),x[2].replace('\"','').replace("\'",'')))
+		 .cache())
+
+#create a groupsRdd. Each tuple of this Rdd will be (fromURL, (Python iterator of all the URLs which be reached from this URL))
+groupRdd=linksFrmToRdd.groupByKey().cache()
+
+# create a ranks rdd with the initail ranks of each URL as 1.
+ranks=groupRdd.map(lambda url:(url[0], 1))
+
+# lets contribute the initail contribution of the URLs towards each of the outgoing links
+# The structure of the intermediate Rdd 'groupRdd.join(ranks)' is a set a tuples like (URL, ((Python iterator of all the URLs which be reached from this URL), rank of the url key))  
+# computeContribs function ia a generator function which yields tuples (outgoingURL, rankoftheParentURL/(number of the outgoingURLs))
+contributionRdd= groupRdd.join(ranks)
+			.flatMap(lambda urlcontribs: computeContribs(urlcontribs[1][0], urlcontribs[1][1]))
+
+# Calculate the new ranks
+ranks=contributionRdd.reduceByKey(lambda x,y:x+y).mapValues(lambda rank:rank*0.85 + 0.15)
+
+# Lets iterate this process using a function 'iteratehelper' from pagerank_helper_functions module 
+# Here I have used a hadcoded value 1000 which is the number of iterations we want the page rank algorithm to run. We can take this 
+# argument from the command line
+ranks=iteratehelper(groupRdd, ranks, contributionRdd, 1000)
+
+#display the top 1000 ranked pages!
+for (url, rank) in ranks.takeOrdered(1000, key=lambda url_rank_tuple: -1*url_rank_tuple[1]):
+    print ('URL: %s, Rank: %s' %(url,rank))
+
+sc.stop()
